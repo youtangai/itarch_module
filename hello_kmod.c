579a2ff97edabf846ee6d4df6218398acb972faa
youtangai <youtanagai@gmail.com>
2017-06-09 14:47:43 +0900

    individual hakodate

--- hello_kmod.c
+++ hello_kmod.c
@@ -6,9 +6,22 @@
 #include <linux/uaccess.h>
 
 #define BUFFSIZE 8
+#define CHARSIZE 1
+#define HELLOSIZE 7
 
-struct cdev char_dev;
+struct cdev char_dev_array[BUFFSIZE];
 char kern_buff[BUFFSIZE+1] = "hakodate";
+
+char hello_array[BUFFSIZE][HELLOSIZE] = {
+		"hello0",
+		"hello1",
+		"hello2",
+		"hello3",
+		"hello4",
+		"hello5",
+		"hello6",
+		"hello7"
+};
 
 static int chardev_open(struct inode *inode, struct file *file){
 		printk(KERN_INFO "Device Opended.\n");
@@ -20,41 +33,227 @@ static int chardev_close(struct inode *inode, struct file *file){
 		return 0;
 }
 
-static ssize_t chardev_read(struct file *file, char __user *buf, size_t count, loff_t *loff){
-		if (count < 8) {
-				printk(KERN_INFO "count size smaller than 8\n");
+static ssize_t chardev_read0(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
 				return -ENOSPC;
 		}
-		copy_to_user(buf, kern_buff, BUFFSIZE);
+		copy_to_user(buf, &kern_buff[0], CHARSIZE);
 		return count;
 }
 
-static ssize_t chardev_write(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
-		if (count < 8) {
-				printk(KERN_INFO "count size smaller than 8\n");
+static ssize_t chardev_write0(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
 				return -ENOSPC;
 		}
-		copy_from_user(kern_buff, buf, BUFFSIZE);
-		printk(KERN_INFO "write string = %s\n", kern_buff);
+		copy_from_user(&kern_buff[0], buf, CHARSIZE);
 		return count;
 }
 
-static struct file_operations chardev_fops = {
-        .owner = THIS_MODULE,
-        .open = chardev_open,
-        .read = chardev_read,
-        .release = chardev_close,
-        .write = chardev_write,
+static ssize_t chardev_read1(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[1], CHARSIZE);
+		return count;
+}
+
+
+static ssize_t chardev_write1(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[1], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read2(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[2], CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_write2(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[2], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read3(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[3], CHARSIZE);
+		return count;
+}
+
+
+static ssize_t chardev_write3(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[3], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read4(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[4], CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_write4(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[4], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read5(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[5], CHARSIZE);
+		return count;
+}
+
+
+static ssize_t chardev_write5(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[5], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read6(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[6], CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_write6(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[6], buf, CHARSIZE);
+		return count;
+}
+
+static ssize_t chardev_read7(struct file *file, char __user *buf, size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_to_user(buf, &kern_buff[7], CHARSIZE);
+		return count;
+}
+
+
+static ssize_t chardev_write7(struct file *file, const char __user *buf, const size_t count, loff_t *loff){
+		if (count < CHARSIZE) {
+				printk(KERN_INFO "error:count size smaller than 1\n");
+				return -ENOSPC;
+		}
+		copy_from_user(&kern_buff[7], buf, CHARSIZE);
+		return count;
+}
+
+static struct file_operations chardev_fops_array[BUFFSIZE] = {
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read0,
+				.release = chardev_close,
+				.write = chardev_write0,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read1,
+				.release = chardev_close,
+				.write = chardev_write1,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read2,
+				.release = chardev_close,
+				.write = chardev_write2,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read3,
+				.release = chardev_close,
+				.write = chardev_write3,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read4,
+				.release = chardev_close,
+				.write = chardev_write4,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read5,
+				.release = chardev_close,
+				.write = chardev_write5,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read6,
+				.release = chardev_close,
+				.write = chardev_write6,
+		},
+		{
+				.owner = THIS_MODULE,
+				.open = chardev_open,
+				.read = chardev_read7,
+				.release = chardev_close,
+				.write = chardev_write7,
+		}
 };
 
-dev_t dev = MKDEV(240, 0);
+dev_t dev_array[BUFFSIZE];
 
 static int __init hello_init(void)
 {
-		int err;
-		register_chrdev_region(dev, 1, "hello");
-		cdev_init(&char_dev, &chardev_fops);
-		err = cdev_add(&char_dev, dev, 1);
+		int i = 0;
+		//setup major minor number
+		for (i = 0; i < BUFFSIZE; i++ ){
+				dev_array[i] = MKDEV(240, i);
+		}
+		for (i = 0; i < BUFFSIZE; i++ ){
+				register_chrdev_region(dev_array[i], 1, hello_array[i]);
+				cdev_init(&char_dev_array[i], &chardev_fops_array[i]);
+				cdev_add(&char_dev_array[i], dev_array[i], 1);
+		}
 		printk(KERN_INFO "init cdev.\n");
 		printk(KERN_INFO "setup hakodate!\n");
 		printk(KERN_INFO "Hello World\n");
@@ -63,12 +262,14 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
+		int i = 0;
 		//delete chardevice
-		cdev_del(&char_dev);
+		for (i = 0; i < BUFFSIZE; i++ ){
+				cdev_del(&char_dev_array[i]);
+				unregister_chrdev_region(dev_array[i], 1);
+		}
 
-		//release chardev number
-		unregister_chrdev_region(dev, 1);
-		printk(KERN_INFO "Bey World\n");
+		printk(KERN_INFO "bye world\n");
 		return;
 }
 
